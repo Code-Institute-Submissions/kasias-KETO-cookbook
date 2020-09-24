61d4309287579d2bc7a61a2023cbacc8bcb5d402
Kasia Bee <kas.bogucka@gmail.com>
2020-09-24 14:54:09 +0000

    Rewired the delete button

--- static/js/script.js
+++ static/js/script.js
@@ -6,9 +6,9 @@ $(document).ready(function(){
 
 
 
-$("#edit").click(function(){
+$("#delete-this").click(function(){
     if(confirm("Are you absolutely sure you want to delete this?")){
-        $("#edit").attr("href", "query.php?ACTION=delete&ID='1'");
+        $("#delete-this").attr("href", "query.php?ACTION=delete&ID='1'");
     }
     else{
         return false;
