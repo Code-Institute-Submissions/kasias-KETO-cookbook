a2c3db0e17e0d4953b09cb6ad6e2c57953eb275b
Kasia Bee <kas.bogucka@gmail.com>
2020-09-30 13:02:02 +0000

    Changed the burger icon to Salmon on mobile view, added social media buttons in footer,added modal for user to confirm they want to delete the recipe

--- static/js/script.js
+++ static/js/script.js
@@ -7,14 +7,6 @@ $(document).ready(function(){
 
 
 
-$("#delete-this").click(function(){
-    if(confirm("Are you absolutely sure you want to delete this?")){
-        $("#delete-this").attr("href");
-    }
-    else{
-        return false;
-    }
-});
 
 
 
